0e8f96cd687c87879f09950b2cb4b0618fa097ff
chetandchavan <101565537+chetandchavan@users.noreply.github.com>
2022-04-01 23:58:46 +0530

    Add files via upload

---
+++ Forward.java
@@ -0,0 +1,10 @@
+// print upto 5 number in forword direction
+
+ class Forward {
+    public static void main (String arg []){
+	 // int i =0;
+	  for (int i=0; i <=5;i++){
+		  System.out.print(i);
+	  }
+	}
+ }
